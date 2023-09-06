8873a190d7e6394afdf4c1e8bf923896033be8ff
whitok355 <3139139@mail.ru>
2023-09-06 11:54:41 +0300

    true init commit

--- TypeWorkComporator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class TypeWorkComporator implements Comparator<Employee> {
-
-    @Override
-    public int compare(Employee o1, Employee o2){
-        return Boolean.compare(o1.freelanceValue, o2.freelanceValue);
-    }
-}
